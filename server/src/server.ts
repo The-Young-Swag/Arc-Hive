df901f3928631a4b0d7ce58cbe94e4640287a48e
the-young-swag <xplizit.nm7@gmail.com>
2026-08-18 16:50:09 +0800

    chore(server): configure environment variables

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = 3000;
+const PORT =  Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
     console.log(`Arc-Hive server running on http://localhost: ${PORT}`);
